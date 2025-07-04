5b9dff57ca84d4dfb8f006b495e66d7b2c307652
Karturbis <118579678+Karturbis@users.noreply.github.com>
2025-07-04 04:08:08 +0200

    add character class

---
+++ character.py
@@ -0,0 +1,7 @@
+class Character():
+
+    def __init__(self, shape:str, length: int, width: int, color:str):
+        self.shape = shape
+        self.length = length
+        self.width = width
+        self.color = color
